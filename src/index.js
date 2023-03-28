15f5a1e2660be30962ad710ac845860b9246e536
Herly80 <herlyyidam@gmail.com>
2023-03-28 17:18:30 -0300

    se corrigio el detalle que no se actualizaba la hora en el digital, se envolvio todo en un setInterval

--- src/index.js
+++ src/index.js
@@ -21,19 +21,18 @@ let minutes = document.getElementById('minutes')
 let seconds = document.getElementById('seconds')
 let ampm = document.getElementById('ampm')
 
+setInterval(() => {
 let h = new Date().getHours()
 let m = new Date().getMinutes()
 let s = new Date().getSeconds()
 
 let am = h >= 12 ? 'PM' : 'AM'
 
-//convertir de 24h a 12h
-if (h > 12) {
+if (h > 12) {   //convertir de 24h a 12h
     h = h - 12
 }
 
-//agregar cero antes del número de un solo dígito
-h= (h < 10) ? '0' + h :  h
+h= (h < 10) ? '0' + h :  h   //agregar cero antes del número de un solo dígito
 m= (m < 10) ? '0' + m :  m
 s= (s < 10) ? '0' + s :  s
 
@@ -41,3 +40,4 @@ hours.innerHTML = h
 minutes.innerHTML = m
 seconds.innerHTML = s
 ampm.innerHTML = am
+})
